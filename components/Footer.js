3d6baecdff713f65b91cdfacc0a30ae3c81cae4e
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-04-21 05:37:02 +1000

    footer social icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,26 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className='w-full h-320 bg-navy-dark text-navy-light flex flex-col place-content-center items-center gap-4 p-16 max-[600px]:p-10 font-medium'>
-      Copyright © Sabrina Nahabedian
+      <div className='flex flex-row gap-2'>
+        <Link
+          href={'https://www.linkedin.com/in/sabrina-nahabedian/'}
+          target='_blank'
+          className='text-3xl mr-auto font-semibold social'
+        >
+          <Image src={'/linkedin_icon.png'} alt={'linkedin'} width={'35'} height={'35'} />
+        </Link>
+        <Link
+          href={'https://github.com/thewildc4rd'}
+          target='_blank'
+          className='text-3xl mr-auto font-semibold social'
+        >
+          <Image src={'/github_icon.png'} alt={'github'} width={'35'} height={'35'} />
+        </Link>
+      </div>
+      <div>Copyright © Sabrina Nahabedian</div>
     </footer>
   );
 }
