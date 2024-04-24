b25f4b805da6e9a3cbfeb8f4f7410b9b2b924875
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-04-24 23:44:39 +1000

    bounce animation on down arrow

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main className='home h-full w-full'>
       <HomeSky />
-      <div className='h-screen w-full flex flex-col justify-center items-center p-10 bg-space-cadet-dark'>
-        !
+      <div className='h-screen w-screen flex flex-col justify-center items-center p-10 bg-space-cadet-dark'>
+        <div></div>
       </div>
     </main>
   );
--- components/HomeSky.js
+++ components/HomeSky.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion, useAnimationControls, useScroll, animate } from 'framer-motion';
 import { useEffect } from 'react';
+import Image from 'next/image';
 
 const HomeSky = (props) => {
   const controls = useAnimationControls();
@@ -18,36 +19,42 @@ const HomeSky = (props) => {
       ['.cloud-1', { x: 100 }, { at: 0.025 }, { duration: 0.5 }],
       ['.cloud-3', { x: -150 }, { at: 0.05 }, { duration: 0.5 }],
       ['.shooting-star-3', { x: -700 }, { at: 0 }, { duration: 0.5 }],
-      ['.shooting-star-2', { x: -1600 }, { at: 0.03 }, { duration: 0.5 }],
+      ['.shooting-star-2', { x: -1000 }, { at: 0.02 }, { duration: 0.1 }],
       ['.shooting-star-1', { x: -500 }, { at: 0.04 }, { duration: 0.1 }],
     ]);
     controls.current.pause();
   }, []);
 
   return (
-    <div className='h-screen w-screen relative'>
-      <div className='sky h-screen w-screen flex flex-col justify-start items-center p-10 absolute -z-10'>
-        <h2 className='mt-20 text-2xl bg-glaucous bg-opacity-85 hover:bg-opacity-100 px-10 py-2 rounded-2xl shadow-xl hover:-translate-y-2 transition-all hover:-rotate-3'>
-          Hi, my name is
-        </h2>
-        <h1 className='text-9xl text-sky-magenta'>Sabrina</h1>
-        {[1, 2, 3].map((num) => (
-          <motion.img
-            key={num}
-            src={`/cloud${num}.png`}
-            alt={'cloud'}
-            width={'500'}
-            height={'500'}
-            className={`cloud cloud-${num} transition-all`}
-          ></motion.img>
-        ))}
-        {[1, 2, 3].map((num) => (
-          <motion.span
-            key={num}
-            className={`shooting-star shooting-star-${num} transition-all`}
-          ></motion.span>
-        ))}
-      </div>
+    <div className='sky h-[94svh] w-screen relative flex flex-col justify-start items-center p-10'>
+      <h2 className='mt-20 text-2xl bg-glaucous bg-opacity-85 hover:bg-opacity-100 px-10 py-2 rounded-2xl shadow-xl hover:-translate-y-2 transition-all hover:-rotate-3'>
+        Hi, my name is
+      </h2>
+      <h1 className='text-9xl text-sky-magenta'>Sabrina</h1>
+      {[1, 2, 3].map((num) => (
+        <motion.img
+          key={num}
+          src={`/cloud${num}.png`}
+          alt={'cloud'}
+          width={'500'}
+          height={'500'}
+          className={`cloud cloud-${num} transition-all`}
+        />
+      ))}
+      {[1, 2, 3].map((num) => (
+        <motion.span
+          key={num}
+          className={`shooting-star shooting-star-${num} transition-all`}
+        ></motion.span>
+      ))}
+      <Image
+        src={'/down_arrow.svg'}
+        alt={'down arrow'}
+        width={'40'}
+        height={'40'}
+        className='absolute bottom-20 z-10'
+        id='down-arrow'
+      />
     </div>
   );
 };
