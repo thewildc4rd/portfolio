e92f157de48d952faaa856f01034ec9ca30b43d4
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-04-26 00:42:47 +1000

    fixing up home sky copy

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
 import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 const inter = Inter({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+});
+
+const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
 });
@@ -20,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${inter.className} min-h-screen flex-col flex bg-space-cadet-dark text-lavender overflow-x-hidden`}
+        className={`${poppins.className} min-h-screen flex-col flex bg-space-cadet-dark text-lavender overflow-x-hidden`}
       >
         <Navbar />
         <div className='flex-1'>{children}</div>
