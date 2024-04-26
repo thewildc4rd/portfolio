41012e71cb52200ecfee1541f51c52651097ec0a
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-04-27 02:06:22 +1000

    start on projects

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HomeSky from '@/components/HomeSky';
 import HomeAboutMe from '@/components/HomeAboutMe';
 import HomeSkills from '@/components/HomeSkills';
+import HomeProjects from '@/components/HomeProjects';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HomeSky />
       <HomeAboutMe />
       <HomeSkills />
+      <HomeProjects />
     </main>
   );
 }
---
+++ components/HomeProjects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeProjects = (props) => {
+  return (
+    <div className='h-[70svh] w-screen flex flex-col justify-start items-center px-24 py-14 gap-10 bg-space-cadet-dark'>
+      <h2 className='text-3xl font-semibold z-10'>Projects</h2>
+      {/* <div className='w-full h-72 bg-space-cadet-light'>project 1</div> */}
+    </div>
+  );
+};
+
+export default HomeProjects;
