6b0fbad880c06166aa3b18fe2d7ffc81b59b1849
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-04-28 04:48:53 +1000

    fixing typos

--- components/About.js
+++ components/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
   return (
     <div
       id='about'
-      className='section h-[75svh] w-screen flex flex-row justify-center px-10 py-14 gap-10 bg-space-cadet-dark max-lg:flex-col max-lg:h-fit'
+      className='section h-[72svh] w-screen flex flex-row justify-center px-10 py-14 gap-10 bg-space-cadet-dark max-lg:flex-col max-lg:h-fit'
     >
       <AboutPhoto />
       <AboutText />
--- components/Experience.js
+++ components/Experience.js
@@ -6,7 +6,7 @@ const Expierence = (props) => {
       id='experience'
       className='section h-[90svh] w-screen flex flex-col justify-start items-center px-24 py-14 gap-10 bg-space-cadet-dark'
     >
-      <h2 className='text-3xl font-semibold z-10'>Expierence</h2>
+      <h2 className='text-3xl font-semibold z-10'>Experience</h2>
     </div>
   );
 };
