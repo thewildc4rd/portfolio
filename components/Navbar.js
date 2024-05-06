7e0078cc8fc609f7526769f6a32e30414c8b3423
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-05-07 00:26:59 +1000

    change hrefs for nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -93,7 +93,7 @@ export default function Navbar() {
       {path === '/' && (
         <>
           <Link
-            href={'/'}
+            href={''}
             scroll={false}
             onClick={() => {
               if (open) {
@@ -131,7 +131,7 @@ export default function Navbar() {
                   setSelectedId(link.id);
                 }}
                 scroll={false}
-                href={'/'}
+                href={''}
               >
                 {link.title}
               </Link>
