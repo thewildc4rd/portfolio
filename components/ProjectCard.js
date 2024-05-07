f13967bf771e6fc4781c28a223f1d88d3e001601
Sabrina Nahabedian <z5361659@ad.unsw.edu.au>
2024-05-07 13:22:42 +1000

    responsiveness for projects

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,7 +8,7 @@ const ProjectCard = ({ title, skills, description, imgSrc, centerImg, style, onC
 
   return (
     <div
-      className='w-full h-[500px] max-lg:h-[400px] bg-space-cadet-light-trans rounded-3xl p-7 shadow-xl flex flex-col'
+      className='w-full h-fit bg-space-cadet-light-trans rounded-3xl p-7 shadow-xl flex flex-col'
       style={style}
     >
       <div className='flex flex-row'>
@@ -34,10 +34,10 @@ const ProjectCard = ({ title, skills, description, imgSrc, centerImg, style, onC
         </div>
       </div>
       <div className='mb-5'>{description}</div>
-      <div className='w-full h-full overflow-hidden rounded-2xl relative'>
+      <div className='w-full h-[400px] max-lg:h-[280px] max-xm:h-[230px] max-md:h-[130px] max-sm:h-[90px] overflow-hidden rounded-2xl relative'>
         <div
           className={`absolute w-full h-[70%] bg-rose-bonbon bg-opacity-70 top-[15%] p-4 rounded-2xl flex items-center text-3xl hover:bg-opacity-80 transition-all shadow-2xl ${
-            hover ? 'left-1' : 'left-8'
+            hover ? 'left-1' : 'left-8 max-sm:left-6'
           } ${onClick != null && onClick != undefined ? 'cursor-pointer' : ''}`}
           onMouseOver={() => setHover(true)}
           onMouseOut={() => setHover(false)}
@@ -57,6 +57,7 @@ const ProjectCard = ({ title, skills, description, imgSrc, centerImg, style, onC
               viewBox='-52.8 -52.8 435.60 435.60'
               stroke='#FCE7F3'
               strokeWidth='25'
+              className='w-[20px] h-[20px] max-sm:w-[15px] max-sm:h-[15px]'
             >
               <path d='M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z' />
             </svg>
@@ -66,7 +67,7 @@ const ProjectCard = ({ title, skills, description, imgSrc, centerImg, style, onC
         <div
           className={`w-[95%] h-full overflow-hidden rounded-2xl absolute shadow-2xl transition-all duration-200 ${
             centerImg ? 'flex items-center' : ''
-          } ${hover ? 'left-14' : 'left-20'}`}
+          } ${hover ? 'left-14' : 'left-20 max-sm:left-16'}`}
         >
           <img src={imgSrc} width={1095} height={328} alt='Photo of the project' />
         </div>
